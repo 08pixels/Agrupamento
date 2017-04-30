faded20c6b336c7d9c8967251e644f799af9b578
08pixels <luizrodrigomarques@gmail.com>
2017-04-30 01:04:44 -0300

    Tirando o excesso e enfim gerando as combições

---
+++ codigo.py
@@ -0,0 +1,13 @@
+
+class Codigo:
+
+	def __init__(self, problema_id, arquivo, medida_corretude_funcional, medida_complexity, medida_distinct_operands, medida_distinct_operators):
+
+		self.problema_id				= problema_id
+		self.arquivo					= arquivo
+
+		self.medida_corretude_funcional	= medida_corretude_funcional
+		self.medida_complexity			= medida_complexity
+		self.medida_distinct_operators	= medida_distinct_operators
+		self.medida_distinct_operands	= medida_distinct_operands
+		self.propriedades				= []
--- main.py
+++ main.py
@@ -1,95 +1,58 @@
+#!/usr/bin/python
 #coding: utf-8
 
-import os, sys
 import MySQLdb
-# from Codes import Codes
+from codigo import Codigo
 
 conexao = MySQLdb.connect('localhost', 'root', 'root', 'Codigos-Estrutura_e_Dados')
-dataTempFile = 'dataTemp'
-
-# for row in cursor.fetchall():
-# 	#	  corretude_func | complexidade | operador | operando
-# 	print row[7],			row[8], 		 row[14], row[15]
-
-# 	# for col in xrange(len(row)):
-# 	# 	print ' ', row[col],
-
-propertyKeys = {'CORRETUDE'		: 1,
-				'COMPLEXIDADE'	: 2,
-				'OPERADORES'	: 3,
-				'OPERANDOS'		: 4,
-				'JACCARD'		: 5,
-				'TEXT'			: 6,
-				'TREE'			: 7
-				}
-
-propertyID = [0, 7, 8, 14, 15, 1, 2, 3]
-
-n_properties = 7	# quantidade de propriedades
-
-# def generateData():
-# 	global propertyKeys
-# 	cursor = conexao.cursor()
-	
-# 	file = (dataTempFile, 'w')
-
-# 	cursor.execute('SELECT * from programacao_codigo')
-
-# 	for row in cursor.fetchall():
-# 		current_str = ''
-# 		current_str.append(	row[],
-# 							row[],
-# 							row[],
-# 							)
-
-# 	cursor.execute('SELECT * from programacao_similaridade')
-
-def createGroups(n_properties):
-	groups = []
-
-	for i in xrange(n_properties + 1):
-		groups.append([])
-
-	return groups
-
-
-def generateMasks(n_properties):
-	masks = []
-
-	for x in range(1, 2**n_properties):
-		masks.append(''.join(str((x>>i)&1) for i in xrange(n_properties-1,-1,-1)))
-
-	return masks
-
-
-# def extract(bitmask):
-# 	global n_properties, cursor
-# 	current = []
-
-# 	for i in xrange(n_properties):
-		
-# 	for row in cursor_1.fetchall():
-# 		if bitmask[i]:
-
-# 			if i < 4:
-# 			else:
-		
-# 				current.append(row[ propertyID[ i + 1 ] ])
-
-
-# 	return current
-
-# masks 	= generateMasks(n_properties)
-# groups	= createGroups(n_properties)
-
-
-# for bitmask in masks:
-# 	groups[ bitmask.count('1') ].append(extract(bitmask))
-
 cursor = conexao.cursor()
+
+n_propriedades = 4	# quantidade de propriedades
+
+propertyKeys = { 0: 'CORRETUDE',
+				 1: 'COMPLEXIDADE',
+				 2: 'OPERADORES',
+				 3: 'OPERANDOS'		}
+
+def identificarChaves():
+	propriedades = [[]]
+
+	for x in range(1, 2**n_propriedades):
+		p = []
+
+		for i in range(0, n_propriedades):
+			if( (x >> i) &1):
+				p.append(propertyKeys[i])
+
+		propriedades.append(p)
+
+	return propriedades
+
+
+def criarCombinacoes(tupla):
+	lista_propriedades = [[n_propriedades]]
+
+	for x in range(1, 2**n_propriedades):
+		propriedade = []
+
+		for i in range(0, n_propriedades): # notar que o 'i' eh a chave (bitmask) referente a cadeia de propriedades
+			if( (x >> i) &1):
+				propriedade.append(tupla[i])
+
+		lista_propriedades.append(propriedade)
+
+	return lista_propriedades
+	
 # cursor.execute('SELECT * FROM `Codigos-Estrutura_e_Dados`.programacao_codigo as codigo WHERE codigo.problema_id = 49')
-cursor.execute('SELECT medidas_ok, problema_id, arquivo, medida_corretude_funcional, medida_complexity, medida_distinct_operands, medida_distinct_operators FROM programacao_codigo')
-# cursor.execute('SELECT * FROM programacao_codigo')
+
+cursor.execute('SELECT medidas_ok, problema_id, arquivo, medida_corretude_funcional, medida_complexity, medida_distinct_operands, medida_distinct_operators FROM programacao_codigo WHERE programacao_codigo.problema_id = 49')
+
+DESCRICAO_DAS_CHAVES = identificarChaves()
+codes = []
 
 for row in cursor.fetchall():
-	print(row)
+
+	if row[0]: # medidas ok
+		code = Codigo(row[1], row[2], row[3], row[4], row[5], row[6]) # segue a ordem de busca
+		code.propriedades = criarCombinacoes(row[3:]) # apartir da terceira posicao comeca a ser os dados das propriedades
+		codes.append(code)
